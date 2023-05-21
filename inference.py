8887a207bfaec8a37c82b988b36be993a8374edc
qvuer7 <qvuer7@gmail.com>
2023-05-21 21:45:42 +0100

    revealed input reshaping added

--- inference.py
+++ inference.py
@@ -3,19 +3,22 @@ from yolo_inferencing.PreProcessor import PreProcessor
 from yolo_inferencing.EngineProcessor import EngineProcessor
 
 import cv2
+image = cv2.imread('humans.jpg')
+print(f'IMAGE SHAPE: {image.shape}')
+
+img_height, img_width = image.shape[:2]
 
 
-ep = EngineProcessor('/home/andrii/GUS/SegmentationOn2GB_08_02_2023/networks/segmentation/yolo/model.engine')
+ep = EngineProcessor('/home/andrii/GUS/SegmentationOn2GB_08_02_2023/networks/segmentation/yolov8.engine')
 prp = PreProcessor()
-pop = PostProcessor(iou_threshold = 0.4, class_threshold = 0.4,
-                 input_height = 416, input_width = 320, img_height = 416, img_width = 320,
+pop = PostProcessor(iou_threshold = 0.3, class_threshold = 0.1,
+                 input_height = 320, input_width = 640, img_height = 320, img_width = 640,
                   num_masks = 32)
 ep.initalize()
 
 
 
-image = cv2.imread('humans.jpg')
-print(f'IMAGE SHAPE: {image.shape}')
+
 
 image_data = prp.process(image)
 output = ep.process(image_data)
