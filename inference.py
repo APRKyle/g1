3cfd0f2e2d7f7c57fbbfafa2c9f09e1192e623e5
qvuer7 <qvuer7@gmail.com>
2023-05-21 21:07:35 +0100

    changed inference.py error

--- inference.py
+++ inference.py
@@ -18,5 +18,5 @@ image = cv2.imread('humans.jpg')
 print(f'IMAGE SHAPE: {image.shape}')
 
 image_data = prp.process(image)
-output = ep.process(image)
+output = ep.process(image_data)
 boxes, masks = pop.process(output)
