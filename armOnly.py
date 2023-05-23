ec864fb3ede188902698ea825a587ce3343f199e
qvuer7 <qvuer7@gmail.com>
2023-05-23 14:10:35 +0100

    revealed input reshaping added

--- armOnly.py
+++ armOnly.py
@@ -63,7 +63,7 @@ try:
             image[x, y, 2] = 150
 
         if spear is not None:
-            print(f' first arg: {(spear[0, 1], spear[0,0])}')
+            print(f' spear: {spear}')
             cv2.circle(image, (spear[0, 1], spear[0,0]), 1, (0,255,0), 2)
             cv2.circle(image, (spear[1, 1], spear[1,0]), 1, (0,255,0), 2)
             cv2.line(image, (spear[0, 1], spear[1, 0]), (255,0,0), 1)
