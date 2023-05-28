32a60ccbf4b1880c1c3d5591b2f5409232a60edc
qvuer7 <qvuer7@gmail.com>
2023-05-28 14:36:35 +0100

    revealed input reshaping added

--- Streamer.py
+++ Streamer.py
@@ -1,43 +1,32 @@
 import cv2, struct, socket
 from subs.Camera import Camera
+from subs.VideoInterface import Streamer
 import time
 
 cam = Camera()
 cam.initCamera()
 
-
-def send_frame(frame, sock):
-    # Serialize the frame as a string
-    frame_data = cv2.imencode('.jpg', frame)[1].tostring()
-
-    # Get the size of the frame
-    size = len(frame_data)
-
-    # Pack the size and frame data as a struct
-    data = struct.pack('<L', size) + frame_data
-
-    # Send the frame data over the socket
-    sock.sendall(data)
+output = Streamer(ip = '192.168.1.232', port = 5000)
+output.initStreamer()
 
 
 
-receiver_ip = '192.168.1.232'
-receiver_port = 5000
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((receiver_ip, receiver_port))
+
+
+
 
 try:
     while True:
         image, _, _ = cam.getData()
-        time.sleep(1)
-        send_frame(image, sock)
+        output.Render(image)
+
 except KeyboardInterrupt:
     cv2.destroyAllWindows()
 
     # Close the socket
-    sock.close()
+    output.close()
 
 
 
--- subs/VideoInterface.py
+++ subs/VideoInterface.py
@@ -1,5 +1,7 @@
 import jetson.utils
-
+import socket
+import cv2
+import struct
 
 
 
@@ -27,6 +29,22 @@ class Output:
             self.videoSaver.Render(output)
 
 
+class Streamer:
+    def __init__(self, ip = '192.168.1.232', port = 5000):
+        self.receiver_ip =  ip
+        self.receiver_port = port
 
+    def initStreamer(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((self.receiver_port, self.receiver_port))
+
+    def Render(self, image):
+        frame_data = cv2.imencode('.jpg', image)[1].tostring()
+        size = len(frame_data)
+        data = struct.pack('<L', size) + frame_data
+        self.sock.sendall(data)
+
+    def close(self):
+        self.sock.close()
 
 
