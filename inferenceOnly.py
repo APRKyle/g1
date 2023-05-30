8fa5f8670123020cf39ab2ad91d25a8b30f1205d
qvuer7 <qvuer7@gmail.com>
2023-05-30 15:52:14 +0100

    revealed input reshaping added

--- inferenceOnly.py
+++ inferenceOnly.py
@@ -53,7 +53,7 @@ try:
 
             if len(spears) != 0:
                 image = viz.process(image, spears)
-        image[unreachable[0], unreachable[1], :] = 0
+        # image[unreachable[0], unreachable[1], :] = 0
         output.Render(image)
 
 except Exception as e:
