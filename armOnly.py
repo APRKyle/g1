81c192b1fb7f1bcb06fbe6dcc696d2b07ed03db9
qvuer7 <qvuer7@gmail.com>
2023-05-31 13:56:44 +0100

    revealed input reshaping added

--- armOnly.py
+++ armOnly.py
@@ -20,7 +20,7 @@ prp = PreProcessor()
 pop = PostProcessor(iou_threshold = 0.8, class_threshold = 0.85,
                  input_height = 480, input_width = 640, img_height = 480, img_width = 640,
                   num_masks = 32)
-asparagusProcessor = AsparagusProcessor(topk = 0.06, botk = 0.06, camera = camera)
+asparagusProcessor = AsparagusProcessor(topk = 0.15, botk = 0.06, camera = camera)
 pather = Pather(min_lenght = 0, min_dist = 100000)
 viz = Vizualizer()
 coms = Communicator(nav_required=False, arm_required=True)
