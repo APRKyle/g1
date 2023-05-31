7eb2310a6de702932fd3d458488bbe3cf0b51c01
qvuer7 <qvuer7@gmail.com>
2023-05-31 12:08:07 +0100

    revealed input reshaping added

---
+++ calibration/QRTracker.py
@@ -0,0 +1,37 @@
+import cv2
+from pyzbar.pyzbar import decode
+
+
+class Tracker:
+    def __init__(self, camera):
+        self.camera = camera
+        self.qrDecoder = cv2.QRCodeDetector()
+
+    def get_coordinate(self):
+        image = self.camera.image
+        barcode = decode(image)
+        mid_x = barcode[0][2][0] + int(barcode[0][2][2] / 2)
+        mid_y = barcode[0][2][1] + int(barcode[0][2][3] / 2)
+        coord = self.camera._calculatePix3D([mid_x, mid_y])
+        coord = list(map(lambda x: int(round(x, 3) * 1000), coord))
+        return coord
+
+if __name__ == '__main__':
+    from subs.Camera import Camera
+    from subs.VideoInterface import Streamer
+
+    camera = Camera()
+    tracker = Tracker(camera = camera)
+    camera.initCamera()
+    output = Streamer(ip = '192.168.1.108')
+
+    while True:
+        camera.getData()
+        c = tracker.get_coordinate()
+        output.Render(camera.image)
+
+
+
+
+
+
