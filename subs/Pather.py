9bc462f8f3450c0d0313990c6a4a55e84d6182b0
qvuer7 <qvuer7@gmail.com>
2023-06-07 11:53:28 +0100

    revealed input reshaping added

--- subs/Pather.py
+++ subs/Pather.py
@@ -56,9 +56,10 @@ class Pather:
             print(f'distance: {distance}')
             if spear.lenght > self.min_length:
                 if distance < self.min_dist:
-                    efficient_spear2d = efficient_spear2d
-                    efficient_spear3d = botArm
-                    stop_signal = True
+                    if cd < 820:
+                        efficient_spear2d = efficient_spear2d
+                        efficient_spear3d = botArm
+                        stop_signal = True
 
         return stop_signal, efficient_spear2d, efficient_spear3d
 
