b8c108f5d011dfcdc6276f74a0b2195c22dec92e
qvuer7 <qvuer7@gmail.com>
2023-06-07 11:44:20 +0100

    revealed input reshaping added

--- subs/Pather.py
+++ subs/Pather.py
@@ -49,6 +49,8 @@ class Pather:
             if np.all(spear.bot_3d == 0):
                 print('unreachable depth')
                 continue
+            cd = np.linalg.norm(spear.bot_3d)
+            print(f'camera distance: {cd}')
             botArm = self._transformIntoRobot(spear.bot_3d)
             distance = np.linalg.norm(botArm)
             print(f'distance: {distance}')
