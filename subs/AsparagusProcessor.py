d2bf8d09d18f522f5aeb2467683fc76c046e15ac
qvuer7 <qvuer7@gmail.com>
2023-07-30 15:44:58 +0100

    top bot pick of points rearanged videoServer on flask added

--- subs/AsparagusProcessor.py
+++ subs/AsparagusProcessor.py
@@ -20,19 +20,15 @@ class AsparagusProcessor:
 
             asparagusMask = np.where(mask == 1)
 
-            print(f'mask shape: {mask.shape}')
+
             asparagus = np.array([asparagusMask[0], asparagusMask[1]])
-            print(f'0 min: {asparagus[0].min()} max: {asparagus[0].max()}')
-            print(f'1 min: {asparagus[1].min()} max: {asparagus[1].max()}')
-            length2d = box[3] - box[1]
-            bot_part = asparagus[:, asparagus[0] > box[3] - length2d * self.botk]
-            #length2d = asparagus[0].max() - asparagus[0].min()
-            #bot_part = asparagus[:, asparagus[0] > asparagus[0].max() - length2d*self.botk]
-            #same with top part buth change max to min
+            #asparagus: 0 -  y coordinate, 1 - x coordinate
+
+            length2d = asparagus[0].max() - asparagus[0].min()
+            bot_part = asparagus[:, asparagus[0] > asparagus[0].max() - length2d * self.botk]
+            top_part = asparagus[:, asparagus[0] < asparagus[0].min() + length2d * self.topk]
 
             bot_point = np.mean(bot_part, axis=1).astype(np.int)[::-1]
-
-            top_part = asparagus[:,asparagus[0] < box[1] + length2d * self.topk]
             top_point = np.mean(top_part, axis=1).astype(np.int)[::-1]
 
 
