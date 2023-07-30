2750e5ed74b16e3dae0c653e26dbfd7967be6e6b
qvuer7 <qvuer7@gmail.com>
2023-07-30 16:25:34 +0100

    point cloud working

--- subs/Camera.py
+++ subs/Camera.py
@@ -76,11 +76,9 @@ class Camera:
 
         self.points.map_to(self.imageRS)
         pointcloud_data = self.points.calculate(self.depthRS)
-        vertices = np.asanyarray(pointcloud_data.get_vertices()).tolist()
+        vertices = np.asanyarray(pointcloud_data.get_vertices())
+        vertices = np.array([list(item) for item in vertices])
 
-        print('-'*20)
-        print(vertices)
-        print('-'*20)
         # Filter out invalid points (where z = 0)
         mask = vertices[:, 2] > 0
         valid_points = vertices[mask]
