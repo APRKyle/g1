dcc060c1f5461e5465c4d9d04ed88c557757868c
qvuer7 <qvuer7@gmail.com>
2023-07-31 15:01:43 +0100

    Asparagus processor debugged

--- subs/AsparagusProcessor.py
+++ subs/AsparagusProcessor.py
@@ -4,10 +4,11 @@ from .Spear import Spear
 
 
 class AsparagusProcessor:
-    def __init__(self, camera, topk = 0.06, botk = 0.06):
+    def __init__(self, camera, topk = 0.06, botk = 0.06, ignore_distance = 650):
         self.topk = topk
         self.botk = botk
         self.camera = camera
+        self.ignore_distance = ignore_distance
 
 
     def process(self, boxes, masks):
@@ -15,22 +16,24 @@ class AsparagusProcessor:
 
         for idx, (box, mask) in enumerate(zip(boxes, masks)):
 
-            mask[np.logical_and(mask == 1, self.camera.depthNP == 0)] = 0
+            mask[np.logical_and(mask == 1, self.camera.depthNP == 0)] = 0  # stereo unreachable pixels distance check
+            mask[np.logical_and(mask == 1, self.camera.depthNP > self.ignore_distance)] = 0
+
+            if np.all(np.all(mask == 0)):
+                continue
             mask = mask.astype(np.int)
 
             asparagusMask = np.where(mask == 1)
 
             asparagus = np.array([asparagusMask[0], asparagusMask[1]])
+            # asparagus: 0 -  y coordinate, 1 - x coordinate
 
-            length2d = box[3] - box[1]
-
-            bot_part = asparagus[:, asparagus[0] > box[3] - length2d * self.botk]
+            length2d = asparagus[0].max() - asparagus[0].min()
+            bot_part = asparagus[:, asparagus[0] > asparagus[0].max() - length2d * self.botk]
+            top_part = asparagus[:, asparagus[0] < asparagus[0].min() + length2d * self.topk]
 
             bot_point = np.mean(bot_part, axis=1).astype(np.int)[::-1]
-
-            top_part = asparagus[:,asparagus[0] < box[1] + length2d * self.topk]
             top_point = np.mean(top_part, axis=1).astype(np.int)[::-1]
-
 
             bot_point3d = self.camera._calculatePix3D(bot_point)
             top_point3d = self.camera._calculatePix3D(top_point)
