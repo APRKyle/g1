b38fc0203e4a22fd1078af8cde9cc5eed921a4e7
qvuer7 <qvuer7@gmail.com>
2023-08-06 21:52:53 +0100

    receiver file added

--- pointCloud.py
+++ pointCloud.py
@@ -49,8 +49,13 @@ def split_into_n_pices(n , indexes, topk, botk):
     res = []
     for i in range(n):
         part = indexes[:, np.logical_and(indexes[0] >=c, indexes[0] <= c + step)]
-        print(part)
+
+        leftmost_point = part[:, np.argmin(part[0])]
+        rightmost_point = part[:, np.argmax(part[0])]
+
         res.append(np.mean(part, axis = 1).astype(np.int)[::-1].tolist())
+        res.append(leftmost_point)
+        res.append(rightmost_point)
         c = c + step
 
     bot_part = indexes[:, indexes[0] > indexes[0].max() - dist * botk]
