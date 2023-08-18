aacf08c62a1d22b55dea3faaa40f3b10a16a198b
qvuer7 <qvuer7@gmail.com>
2023-08-18 14:04:39 +0100

    receiver file added

--- armOnly.py
+++ armOnly.py
@@ -21,7 +21,7 @@ pop = PostProcessor(iou_threshold = 0.8, class_threshold = 0.85,
                  input_height = 480, input_width = 640, img_height = 480, img_width = 640,
                   num_masks = 32)
 asparagusProcessor = AsparagusProcessor(topk = 0.02, botk = 0.02, camera = camera)
-pather = Pather(min_lenght = 0, min_dist = 250)
+pather = Pather(min_lenght = 0, min_dist = 400)
 viz = Vizualizer()
 coms = Communicator(nav_required=False, arm_required=True)
 coms.initComs()
