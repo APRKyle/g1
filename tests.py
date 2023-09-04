152771514e4744e932300d4963f79772c5d0be63
qvuer7 <qvuer7@gmail.com>
2023-09-04 12:29:23 +0100

    receiver file added

--- tests.py
+++ tests.py
@@ -20,7 +20,7 @@ prp = PreProcessor()
 pop = PostProcessor(iou_threshold = 0.8, class_threshold = 0.85,
                  input_height = 480, input_width = 640, img_height = 480, img_width = 640,
                   num_masks = 32)
-asparagusProcessor = AsparagusProcessor(topk = 0.15, botk = 0.09, camera = camera)
+asparagusProcessor = AsparagusProcessor(topk = 0.15, botk = 0.09, camera = camera, n_skeletons = 15, ignore_distance=400)
 pather = Pather(min_lenght = 0, min_dist = 0, max_distance = 1000000)
 viz = Vizualizer()
 coms = Communicator(nav_required=False, arm_required=True)
