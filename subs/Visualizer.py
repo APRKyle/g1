d8da331156964274e4e3534d63a50e2fe5dd1d3d
qvuer7 <qvuer7@gmail.com>
2023-09-04 15:19:24 +0100

    receiver file added

--- subs/Visualizer.py
+++ subs/Visualizer.py
@@ -65,9 +65,9 @@ class Vizualizer:
 
             position = (x, y)
             font = cv2.FONT_HERSHEY_SIMPLEX
-            font_scale = 1
+            font_scale = 0.3
             font_color = (255, 255, 255)
-            font_thickness = 2
+            font_thickness = 0.8
             cv2.putText(image, text, position, font, font_scale, font_color, font_thickness)
 
         return image
