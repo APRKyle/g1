ec26c84bc46e39afdabee72e8ee2261dd231ac9f
qvuer7 <qvuer7@gmail.com>
2023-09-13 18:12:30 +0100

    EndEffector PathPlanning planner_test added

---
+++ planner_test.py
@@ -0,0 +1,72 @@
+from subs.Factory import assemble
+
+import time
+import traceback
+
+ep, prp, pop, asparagusProcessor, pather, viz, coms, camera, output, endEffector, planner = assemble(config_path ='config.yaml')
+print('CLASSES LOADED')
+
+
+try:
+    while True:
+
+
+        data = {}
+        camera.getData()
+        image = camera.image
+        image_data = prp.process(image)
+        net_output = ep.process(image_data)
+        boxes, masks, classid = pop.process(net_output)
+
+        if len(classid) != 0:
+
+
+            spears, data = asparagusProcessor.process(boxes, masks) #filtered on camera reach and camera distance
+            print(data)
+
+
+
+
+            #image = viz.process_tests(image, data1)
+
+        output.renderImage(image)
+
+except Exception as e:
+    traceback.print_exc()
+    ep.deinitialize()
+finally:
+    ep.deinitialize()
+
+'''
+try:
+    while True:
+
+
+        data = {}
+        camera.getData()
+        image = camera.image
+        image_data = prp.process(image)
+        net_output = ep.process(image_data)
+        boxes, masks, classid = pop.process(net_output)
+
+        if len(classid) != 0:
+
+
+            spears, data = asparagusProcessor.process(boxes, masks)
+            print(spears)
+
+
+
+
+            if len(spears) != 0:
+                image = viz.process(image, spears)
+
+        output.Render(image, data)
+
+except Exception as e:
+    traceback.print_exc()
+    ep.deinitialize()
+finally:
+    ep.deinitialize()
+
+'''
---
+++ subs/PathPlanning.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+
+
+class BrutePlanner:
+    def __init__(self, end_effector, min_angle = -90, max_angle = 90, resolution = 5):
+        self.ef = ef
+        self.min_angle = min_angle
+        self.max_angle = max_angle
+        self.resolution = resolution
+        self.n_steps = np.ceil((max_angle - min_angle) / resolution).astype(int)
+
+    def _sort_spears(self, spears):
+        sorting_key = lambda x: np.linalg.norm(x.bot_3d - self.ef.blade)
+        data = sorted(spears.spears, key=sorting_key)
+        return data
+
+    def process(self, data):
+        success = False
+        ideal_angle = 0
+
+        for spear_idx, spear_target in enumerate(data):
+            target = spear_target.bot_3d
+            self.ef.goto(target, self.min_angle, 'pre')
+            b_idx = spear_idx
+
+            for _ in range(self.n_steps):
+                success = False
+                collisions = [False for i in range(len(data))]
+                self.ef.goto(target, self.resolution, 'pre')
+                cut_points = np.linspace(self.ef.approach_line[0], self.ef.approach_line[-1])
+
+                for p in cut_points:
+                    self.ef.goto(p, pivot_point='blade')
+
+                    for idx, spear in enumerate(data):
+                        if idx == b_idx:
+                            continue
+                        else:
+                            collisions[idx] = self.ef.check_colision(spear.bot_3d)
+
+                    if any(collisions):
+                        break
+                    else:
+                        pass
+                if not any(collisions):
+                    success = True
+                    ideal_angle = self.ef.angle
+                    best_idx = spear_idx
+                    break
+                else:
+                    ideal_angle = None
+        if success:
+            return ideal_angle, target, best_idx
+
+        else:
+            return None, None, None
+
+
+if __name__ == '__main__':
+    pass
