c2132e86225267cf3f3fb6f51e548e19c732ec11
qvuer7 <qvuer7@gmail.com>
2023-09-14 13:48:13 +0100

    config ip changed

--- planner_test.py
+++ planner_test.py
@@ -26,9 +26,10 @@ try:
 
 
             spears, data = asparagusProcessor.process(boxes, masks) #filtered on camera reach and camera distance
-            print(data)
 
-
+            a, target, sid = planner.process(spears)
+            print(a)
+            print(target)
             #image = viz.process_tests(image, data1)
 
         output.renderImage(image)
