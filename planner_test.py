d11ebabc276f0b31913e273026c8fa59e0c06e03
qvuer7 <qvuer7@gmail.com>
2023-09-18 12:10:32 +0100

    added transofrmation to robot frame of pickoff point

--- planner_test.py
+++ planner_test.py
@@ -27,8 +27,9 @@ try:
 
             spears, data = asparagusProcessor.process(boxes, masks) #filtered on camera reach and camera distance
             _ = pather._calc_rob_pickofs(spears)
-            a, target, sid = planner.process(spears)
             print(data)
+            a, target, sid = planner.process(spears)
+
             print(a)
             print(target)
             # for s in spears:
